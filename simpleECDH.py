2ff5d215d88db60e0f320a1f613d7c331c2355be
trbouma <trbouma@gmail.com>
2022-08-26 22:11:34 -0400

    doc string

--- simpleECDH.py
+++ simpleECDH.py
@@ -8,6 +8,7 @@ def make_shared_secret(privkey_local_str : str, pubkey_remote_str: str):
     ''' Simple ECDH - accepts string versions of private key and public key
         privkey_local_str is the hex string of the wallet private key
         pubkey_remote_str is the hex string on the verifier public key
+        returns the ecdh shared secret as a hex string
     '''
 
     privkey_local_obj = PrivateKey(bytes(bytearray.fromhex(privkey_local_str)), raw=True)
